106f9c09e3cd9d5dabfdb36610a79d0dcc3f235c
jboone2 <jboone2@uoregon.edu>
2020-04-04 14:35:59 -0700

    Add alphaPosition to functions.js.

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -1,36 +1,44 @@
 // jshint esversion: 6
-var main = function () {
-};
+var main = function() {};
 
-let arrAvg = (numList) => {
+// Calculates average value of a numeric array.
+let arrAvg = numList => {
   var total = 0;
-  for(i=0; i < numList.length; i++){
+  for (i = 0; i < numList.length; i++) {
     total += numList[i];
-    }
-  return (total/numList.length);
+  }
+  return total / numList.length;
 };
 
-let arrMax = (numList) => {
+// Determines largest number in a numeric array.
+let arrMax = numList => {
   var maximum = numList[0];
-  numList.forEach(function(element){
-    //console.log(element);
-    if (maximum < element){
+  numList.forEach(function(element) {
+    if (maximum < element) {
       maximum = element;
     }
   });
-  return (maximum);
+  return maximum;
 };
 
-let sumEvens = (numList) => {
+// Calcuates sum of all even numbers in a numeric array.
+let sumEvens = numList => {
   var totalEvens = 0;
   for (var element of numList) {
     if (element % 2 == 0) {
       totalEvens += element;
     }
   }
-  return (totalEvens);
+  return totalEvens;
 };
 
-
-//var x = sumEvens([1, 2, 3, 4, 5]);
-//document.getElementById("demo").innerHTML = x;
+// Determines alphabetical position of a char.
+let alphaPosition = function(ch) {
+  var charPosition = ch.charCodeAt(0);
+  if (charPosition >= 65 && charPosition <= 90) {
+    charPosition -= 64;
+  } else if (charPosition >= 97 && charPosition <= 122) {
+    charPosition -= 96;
+  } else charPosition = "Not in the alphabet.";
+  return charPosition;
+};
